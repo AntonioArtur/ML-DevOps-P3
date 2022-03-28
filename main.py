4981b93c83035558eda2385b597ad97d7a50d1e2
Antonio Artur <artur_holanda@hotmail.com>
2022-03-28 20:05:53 -0300

    a

--- main.py
+++ main.py
@@ -2,7 +2,13 @@
 from src.ml.data import *
 from src.ml.model import *
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModelimport os
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 app = FastAPI()
 
